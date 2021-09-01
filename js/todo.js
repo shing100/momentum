8e664b6f5c2bdaf7512e6b5496b5f6666ee0024e
Lim Geun <shing100@naver.com>
2021-09-01 23:55:51 +0900

    save to dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
-
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
@@ -24,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
